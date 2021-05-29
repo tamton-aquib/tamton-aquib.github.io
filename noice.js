b993638818155e6ad2d50fe6c8fb4f314c6b6129
tamton-aquib <aquibjavedt007@gmail.com>
2021-05-29 19:37:26 +0530

    closed for renovation

--- noice.js
+++ noice.js
@@ -1,3 +1,28 @@
 
-console.log("Nice")
+const buttonBox = document.querySelector('.button-box')
+const button = document.querySelector('.button')
+
+const noice = document.querySelector('.container')
+const dark = "#2c2e34"
+const light = "#dddddd"
+
+let isToggled = false
+let count = 0;
+
+button.addEventListener('click', () => {
+	if (count > 3) {
+		document.querySelector('.heading').innerHTML = "Dont you have anything else to do? 🌝"
+	}
+	let alignment = isToggled ? "left" : "right"
+	if (!isToggled) {
+		noice.style.backgroundColor = light
+		buttonBox.style.textAlign = alignment
+	} else {
+		noice.style.backgroundColor = dark
+		buttonBox.style.textAlign = alignment
+	}
+	isToggled = !isToggled
+	count++;
+})
+
 
