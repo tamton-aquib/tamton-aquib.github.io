a12adc00884c17fb2ccc5ad336a6802bfb259627
tamton-aquib <aquibjavedt007@gmail.com>
2021-06-28 20:52:38 +0530

    updating icon quality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,17 +7,17 @@ const About = () => {
 		<ul className="social_links_ul">
 			<li className="social_links_li">
 				<a target="_blank" href="https://discord.com/users/845674119391477820">
-					<img src="https://img.icons8.com/bubbles/50/000000/discord.png" />
+					<img src="https://img.icons8.com/bubbles/100/000000/discord.png" width="50" />
 				</a>
 			</li>
 			<li className="social_links_li">
 				<a target="_blank" href="https://github.com/tamton-aquib">
-					<img src="https://img.icons8.com/bubbles/50/000000/github.png" />
+					<img src="https://img.icons8.com/bubbles/100/000000/github.png" width="50" />
 				</a>
 			</li>
 			<li className="social_links_li">
 				<a target="_blank" href="https://twitter.com/tamton_aquib">
-					<img src="https://img.icons8.com/bubbles/50/000000/twitter-circled.png" />
+					<img src="https://img.icons8.com/bubbles/100/000000/twitter-circled.png" width="50" />
 				</a>
 			</li>
 			<li className="social_links_li">
