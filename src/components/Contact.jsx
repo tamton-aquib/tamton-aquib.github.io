f80c1b8063c179157fbcfa57bf7f566a34f31297
tamton-aquib <aquibjavedt007@gmail.com>
2021-06-28 14:38:14 +0530

    updating pages except about

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,43 @@
 
 import React from 'react';
+import './Contact.css'
 
 const About = () => {
 	return (
-		<h1 style={{ color: 'green' }}>Contact</h1>
+		<ul className="social_links_ul">
+			<li className="social_links_li">
+				<a target="_blank" href="https://discord.com/users/845674119391477820">
+					<img
+						src="/images/discord.svg"
+						width="40"
+					/>
+				</a>
+			</li>
+			<li className="social_links_li">
+				<a target="_blank" href="https://github.com/tamton-aquib">
+					<img
+						src="/images/github.png"
+						width="45"
+					/>
+				</a>
+			</li>
+			<li className="social_links_li">
+				<a target="_blank" href="https://twitter.com/tamton_aquib">
+					<img
+						src="https://img.icons8.com/color/50/000000/twitter-circled.png"
+						width="45"
+					/>
+				</a>
+			</li>
+			<li className="social_links_li">
+				<a target="_blank" href="https://dev.to/tamtonaquib">
+					<img
+						src="https://img.icons8.com/color/48/000000/devpost.png"
+						width="45"
+					/>
+				</a>
+			</li>
+		</ul>
 	);
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,25 @@
 
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import './Home.css'
 
 const Home = () => {
+	const [currentText, setCurrentText] = useState("")
+	const index = useRef(0)
+	const text = "Hello!"
+
+	useEffect(() => {
+		setTimeout(() => {
+			setCurrentText((v) => v + text.charAt(index.current))
+			index.current++
+		}, 200)
+	}, [currentText, text])
+
 	return (
-		<h1 style={{ color: 'green' }}>Home</h1>
+		<div className="home_container">
+			<h1>
+				{currentText}
+			</h1>
+		</div>
 	);
 }
 
