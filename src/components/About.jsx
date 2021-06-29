2a9e521cd923594b121b65dfe21af3729c9a598e
tamton-aquib <aquibjavedt007@gmail.com>
2021-06-29 14:15:12 +0530

    adding some dummy stuff in about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,13 @@
 
 import React from 'react';
+import './About.css'
 
 const About = () => {
 	return (
-		<h1 style={{ color: 'green' }}>About</h1>
+		<div className="About">
+			<img className="pain" src="https://i.kym-cdn.com/photos/images/original/001/821/862/510.jpg" />
+			<p className="About_para">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. </p>
+		</div>
 	);
 }
 
