41b6f5798d0c61427af3dc2ea9474b4da99c1729
tamton-aquib <aquibjavedt007@gmail.com>
2021-09-01 17:59:09 +0530

    a little cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,21 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header.jsx'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import About from './components/About'
-import Home from './components/Home'
+import About from './components/About.jsx'
+import Home from './components/Home.jsx'
 import Contact from './components/Contact.jsx'
 
 function App() {
 
 	return (
-		<>
-			<Router>
-				<Header />
-				<div className="App">
-					<Route path="/" exact component={Home} />
-					<Route path="/about" component={About} />
-					<Route path="/contact" component={Contact} />
-				</div>
-			</Router>
-		</>
+		<Router>
+			<Header />
+			<div className="App">
+				<Route path="/" exact component={Home} />
+				<Route path="/about" component={About} />
+				<Route path="/contact" component={Contact} />
+			</div>
+		</Router>
 	)
 }
 
