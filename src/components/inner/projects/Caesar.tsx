62d96b789acf0691469df9ec779daff9a2f1f2ff
tamton-aquib <aquibjavedt007@gmail.com>
2022-06-20 23:34:28 +0530

    basic caesar but bugs

--- src/components/inner/projects/Caesar.tsx
+++ src/components/inner/projects/Caesar.tsx
@@ -1,10 +1,21 @@
 
 const Caesar = (query: string) => {
-    // let out = "Answer Caesar: " + query;
-    // 
-    // return (<>{out}</>)
-    console.log(query);
-    return "Caesar: " + query
+    const range = (start: number, end: number, length = end - start + 1) => Array.from({ length }, (_, i) => start + i)
+
+    let letters = "abcdefghijklmnopqrstuvwxyz";
+    let results: string;
+    range(1, 25).map((i: number) => {
+        query.split("").map((c) => {
+            if (letters.includes(c)) {
+                let stuff = letters[(letters.indexOf(c) + i) % 26];
+                results += stuff
+            } else {
+                results += c
+            }
+        })
+        results += "\n";
+    });
+    return results
 }
 
 export default Caesar;
