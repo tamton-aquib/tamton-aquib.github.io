2fafdfae876b4388e7555f3cac2e971278fc4059
tamton-aquib <aquibjavedt007@gmail.com>
2022-07-02 21:13:54 +0530

    adding typewrite effect

--- src/lib/MidContainer/Home.svelte
+++ src/lib/MidContainer/Home.svelte
@@ -1,1 +1,18 @@
-<h1>Hello Home!</h1>
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    const text = "Hello There!";
+    let index = 0;
+    let current: string = "";
+
+    onMount(() => {
+        let intervalId = setInterval(() => {
+            if (current.length === text.length) clearInterval(intervalId);
+
+            current += text.charAt(index);
+            index++;
+        }, 250);
+    });
+</script>
+
+<h1>{current}</h1>
